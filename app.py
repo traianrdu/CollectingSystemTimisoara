2f320b30b4532e66cfe9d072f5c0ab6bb3d0c589
Thedragonbetweentheoceans <poenaruiulian2003@gmail.com>
2021-12-04 01:26:12 +0200

    Map

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, flash,session
-from  flask_mysqldb import MySQL
 import bcrypt
 from db import db_init, db
 from models import Users
