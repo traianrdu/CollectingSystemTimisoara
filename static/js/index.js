ee283534c95345e6cc57f11734d68996a36154ca
Traian <traian.plosca@gmail.com>
2021-12-04 11:51:12 +0200

    pages

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 10,
+      zoom: 11,
       center: { lat: 45.713187, lng: 21.27952 },
     });
 
@@ -8,40 +8,23 @@ function initMap() {
 }
 
 
-const beaches = [
-    ["Bondi Beach", 45.755312, 21.228491, 4],
-    ["Coogee Beach", 45.713187, 21.27952, 5],
-    ["Cronulla Beach", -34.028249, 151.157507, 3],
-    ["Manly Beach", -33.80010128657071, 151.28747820854187, 2],
-    ["Maroubra Beach", -33.950198, 151.259302, 1],
+const containers = [
+    ["punct1", 45.755312, 21.228491, "mm_20_green.png"],
+    ["punct2", 45.713187, 21.27952, "mm_20_brown.png"],
   ];
 
 function setMarkers(map) {
-    const image = {
-        url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
 
-        size: new google.maps.Size(20, 32),
-
-        origin: new google.maps.Point(0, 0),
-
-        anchor: new google.maps.Point(0, 32),
-    };
-
-    const shape = {
-        coords: [1, 1, 1, 20, 18, 20, 18, 1],
-        type: "poly",
-      };
-
-      for (let i = 0; i < beaches.length; i++) {
-        const beach = beaches[i];
+      for (let i = 0; i < containers.length; i++) {
+        const container = containers[i];
 
         new google.maps.Marker({
-          position: { lat: beach[1], lng: beach[2] },
+          position: { lat: container[1], lng: container[2] },
           map,
-          icon: image,
-          shape: shape,
-          title: beach[0],
-          zIndex: beach[3],
+          icon: {
+            url: "http://labs.google.com/ridefinder/images/" +  container[3]
+          },
+          title: container[0],
         });
       }
 
