a4d4304e917bb3f08d44101a16ea35f3044b3409
Iulian <54375582+Thedragonbetweentheoceans@users.noreply.github.com>
2021-12-04 22:43:36 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,84 @@
-from flask import Flask, render_template, request, redirect, url_for, flash,session
+import json
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import bcrypt
-from db import db_init, db
-from models import Users
+
+from manager.JsonManager import JsonManager
+from manager.db import db_init, db
+from model.CollectingPoints import CollectingPoints
+from model.JsonDump import JsonDump
+from model.models import Users , Contact, Container
 from datetime import timedelta
 
+load_dotenv()
 app = Flask(__name__)
- 
 
-app.permanent_session_lifetime=timedelta(days=5)
+app.permanent_session_lifetime = timedelta(days=5)
 app.secret_key = "gameover"
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db_init(app)
 
 
-
-
-@app.route("/")
+@app.route("/", methods=['POST','GET'])
 def home():
-    text=""
+    # User session for feed back, the 0 and 1 will be replaced
     if "userInSession" in session:
-        text = "Hello " + session['userInSession']
-    else: 
-        text = "Hello"
-    return render_template("home.html", text=text)
+        1
+    else:
+        0
 
-@app.route("/signup", methods = ['POST' ,'GET'])
-def signup():
+    if request.method == "POST":
+        name = request.form['name']
+        mail = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+
+        
+       
+        saveInDB = Contact(
+            name = name,
+            mail = mail,
+            subject = subject,
+            message = message
+        )                
+        db.session.add(saveInDB)
+        db.session.commit()
+
+
     
+    return render_template("home.html", env_key=os.getenv("GOOGLE_CLOUD_KEY"))
+
+@app.route("/contact", methods=['POST','GET'])
+def contact():
+    if request.method == "POST":
+        name = request.form['name']
+        mail = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+
+        saveInDB = Contact(
+            name = name,
+            mail = mail,
+            subject = subject,
+            message = message
+        )                
+        db.session.add(saveInDB)
+        db.session.commit()
+    return render_template("contact.html")
+
+
+@app.route("/materials")
+def materials():
+    return render_template("materials.html")
+
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
     message = None
 
-    if request.method =="POST":
-
+    if request.method == "POST":
 
         session.permanent = True
 
@@ -42,25 +89,22 @@ def signup():
 
         selectAllUsers = Users.query.filter_by(username=username).first()
 
-        
-        if   not selectAllUsers:
-            
+        if not selectAllUsers:
 
             hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
             if password == retypedPassword:
                 addUserInDB = Users(
-                    username = username,
-                    password = hashed,
-                    mail = mail
+                    username=username,
+                    password=hashed,
+                    mail=mail
                 )
 
-                
                 db.session.add(addUserInDB)
                 db.session.commit()
 
                 session['userInSession'] = username
-                message="Welcome"
-            else: 
+                message = "Welcome"
+            else:
                 message = "The passwords don't match,Try again."
         else:
             message = "The username already exists."
@@ -69,9 +113,9 @@ def signup():
         flash(message)
     return render_template("signup.html")
 
-@app.route("/signin", methods=['POST','GET'])
-def signin():
 
+@app.route("/signin", methods=['POST', 'GET'])
+def signin():
     message = None
 
     if request.method == 'POST':
@@ -81,21 +125,77 @@ def signin():
         selectUserFromDB = Users.query.filter_by(username=username).first()
 
         if selectUserFromDB:
-            if bcrypt.checkpw(password.encode('utf-8'),selectUserFromDB.password):
+            if bcrypt.checkpw(password.encode('utf-8'), selectUserFromDB.password):
                 session["userInSession"] = username
                 message = "Now you are signed in!"
             else:
                 message = "Password incorrect!"
-    
+
     if message != None:
         flash(message)
     return render_template("signin.html")
+
 
 @app.route("/signout")
 def signout():
     if "userInSession" in session:
         session.pop("userInSession", None)
         return redirect(url_for("home"))
+    else:
+        return render_template("home.html")
 
-if __name__== "__main__":
+
+
+@app.route("/emptyCont" , methods=['POST','GET'])
+def empty():
+    if 'userInSession' in session:
+        if request.method == 'POST':
+            nameOfEmptyCan = request.form['name']
+            percentOfCan = request.form['percent']
+            percentOfCan ="perc_" + percentOfCan[:len(percentOfCan)-1]
+
+            selectContainerFromDB =  Container.query.filter_by(name=nameOfEmptyCan).first()
+
+            percents = ['perc_50' , 'perc_70' , 'perc_100']
+
+            for i in range(len(percents)):
+                if percents[i] == percentOfCan:
+                    x = i
+                    break
+            if i == 0:
+                selectContainerFromDB.perc_50 = selectContainerFromDB.perc_50 + 1
+                db.session.commit()
+            elif i == 1:
+                selectContainerFromDB.perc_70 = selectContainerFromDB.perc_70 + 1
+                db.session.commit()
+            elif i == 1:
+                selectContainerFromDB.perc_100 = selectContainerFromDB.perc_100 + 1
+                db.session.commit()
+
+
+            print(nameOfEmptyCan + " " + percentOfCan)
+    return render_template("home.html")
+
+# @app.route("/cityhall")
+# def cityhall():
+#     session["staff"] ="staff"
+#     if "staff" in session:
+
+#         return render_template("cityhallmap.html")
+
+@app.route("/get_collecting_points", methods=['POST', 'GET'])
+def closest_location():
+    json_manager = JsonManager(
+        'https://data.primariatm.ro/api/3/action/datastore_search?resource_id=d0134630-84d9-40b8-9bcb-dfdc926d66ab'
+        '&limit=500')
+    jd = JsonDump.from_json(json_manager.connect())
+    collecting_list = []
+    for item in jd.return_records():
+        cp = CollectingPoints(item["_id"], item["id"], item["tip colectare"], item['adresa'], item['companie'],
+                              item['website'], item['latitudine'], item['longitudine'])
+        collecting_list.append(json.dumps(cp.__dict__))
+    return json.dumps(collecting_list)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,7 @@ function initMap() {
       zoom: 11,
       center: { lat: 45.713187, lng: 21.27952 },
     });
-  
+
     setMarkers(map);
 }
 
@@ -15,18 +15,45 @@ const containers = [
 
 function setMarkers(map) {
 
+      const windows = [];
+
       for (let i = 0; i < containers.length; i++) {
         const container = containers[i];
-    
-        new google.maps.Marker({
+
+        const marker = new google.maps.Marker({
           position: { lat: container[1], lng: container[2] },
           map,
-          icon: {                             
+          icon: {
             url: "http://labs.google.com/ridefinder/images/" +  container[3]
           },
           title: container[0],
         });
+        
+        windows.push(marker);
       }
 
     
+
+      for(let i=0;i<windows.length;i++)
+      {
+          const point = windows[i];
+
+          const infowindow = new google.maps.InfoWindow({
+            content: point.title,
+          });
+
+
+          point.addListener("click", () => {
+
+            infowindow.open({
+              anchor: point,
+              map,
+              shouldFocus: false,
+            });
+
+            document.getElementById("emptyContainerPres").innerHTML = point.title + " is empty: "
+            document.getElementById("emptyContainer").innerHTML = point.title
+
+          });
+      }
 }
